3e24ab029453992dc140f8ad29de571735394ec0
David Jones <davejonesbkk@gmail.com>
2019-03-23 08:54:39 +0700

    Find new links and crawl next

--- crawler.py
+++ crawler.py
@@ -15,19 +15,26 @@ def crawl_bot(url):
 	html = requests.get(url).text
 	soup = BeautifulSoup(html, "html.parser")
 	#Find all a href tags
-	for link in soup.find_all('a'):
-		internal_links.add(link)
-
-	for l in internal_links:
-		print(l)
-		
+	for link in soup.find_all('a', attrs={'href': re.compile("^http://")}):
+		if "href" in link.attrs:
+			print(link)
+			#Check if each found link is in the internal_links set and if not add it
+			if link.attrs["href"] not in internal_links:
+				new_link = link.attrs["href"]
+				print(new_link)
+				internal_links.add(new_link)
+				print("All links found so far, ", internal_links)
+				time.sleep(6)
+				crawl_bot(new_link)
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('url', help='Pass the website you wish to crawl')
+	parser.add_argument('url', help='Pass the website you wish to crawl \
+		please include the http://')
 	args = parser.parse_args()
 
 	url = args.url
+
 
 	crawl_bot(url)
 
